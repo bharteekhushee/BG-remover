37f73b998076627425cca6d0f6562fc7f8d7aa09
bharteekhushee <bharteekhushee@gmail.com>
2025-12-10 15:20:10 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Navbar from './Components/Navbar'
+import Footer from './Components/Footer'
+import Home from './Pages/Home'
+import Result from './Pages/Result'
+
+
+const App = () => {
+  return (
+    <div className='min-h-screen bg-slate-50'>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/result" element={<Result />} />
+          <Route path="*" element={<h1>404 Page not found.</h1>} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+    </div>
+  )
+}
+
+export default App
